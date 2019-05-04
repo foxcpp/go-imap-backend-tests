3a6004619cad5c14501dd3d139c09f4dbfce0fab
fox.cpp <fox.cpp@disroot.org>
2019-05-04 13:33:42 +0300

    Merge and extend search tests

--- mailbox_search.go
+++ mailbox_search.go
@@ -16,53 +16,49 @@ import (
 //
 // Intended for backends using custom search implementation.
 
+var testInternalDate = time.Unix(1483997966, 0)
+
 var matchTests = []struct {
-	name     string
+	flags    []string
+	date     time.Time
 	criteria *imap.SearchCriteria
 	res      bool
 }{
 	{
-		name: "From hdr",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"From": {"Mitsuha"}},
 		},
 		res: true,
 	},
 	{
-		name: "To hdr",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"To": {"Mitsuha"}},
 		},
 		res: false,
 	},
 	{
-		name:     "SentBefore",
 		criteria: &imap.SearchCriteria{SentBefore: testDate.Add(48 * time.Hour)},
 		res:      true,
 	},
 	{
-		name: "SentSince",
 		criteria: &imap.SearchCriteria{
 			Not: []*imap.SearchCriteria{{SentSince: testDate.Add(48 * time.Hour)}},
 		},
 		res: true,
 	},
 	{
-		name: "Body not contains",
 		criteria: &imap.SearchCriteria{
 			Not: []*imap.SearchCriteria{{Body: []string{"name"}}},
 		},
 		res: false,
 	},
 	{
-		name: "Text",
 		criteria: &imap.SearchCriteria{
 			Text: []string{"name"},
 		},
 		res: true,
 	},
 	{
-		name: "Or",
 		criteria: &imap.SearchCriteria{
 			Or: [][2]*imap.SearchCriteria{{
 				{Text: []string{"i'm not in the text"}},
@@ -72,68 +68,53 @@ var matchTests = []struct {
 		res: false,
 	},
 	{
-		name: "Header",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"Message-Id": {"42@example.org"}},
 		},
 		res: true,
 	},
 	{
-		name: "Header#2",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"Message-Id": {"43@example.org"}},
 		},
 		res: false,
 	},
 	{
-		name: "Header#3",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"Message-Id": {""}},
 		},
 		res: true,
 	},
 	{
-		name: "Header#4",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"Reply-To": {""}},
 		},
 		res: false,
 	},
 	{
-		name: "Size Larger",
 		criteria: &imap.SearchCriteria{
 			Larger: 10,
 		},
 		res: true,
 	},
 	{
-		name: "Size Smaller",
 		criteria: &imap.SearchCriteria{
 			Smaller: 10,
 		},
 		res: false,
 	},
 	{
-		name: "Header#5",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"Subject": {"your"}},
 		},
 		res: true,
 	},
 	{
-		name: "Header#6",
 		criteria: &imap.SearchCriteria{
 			Header: textproto.MIMEHeader{"Subject": {"Taki"}},
 		},
 		res: false,
 	},
-}
-
-var flagsTests = []struct {
-	flags    []string
-	criteria *imap.SearchCriteria
-	res      bool
-}{
 	{
 		flags: []string{imap.SeenFlag},
 		criteria: &imap.SearchCriteria{
@@ -177,71 +158,167 @@ var flagsTests = []struct {
 		},
 		res: false,
 	},
+	{
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: new(imap.SeqSet),
+					Not: []*imap.SearchCriteria{{SeqNum: new(imap.SeqSet)}},
+				},
+				{
+					SeqNum: new(imap.SeqSet),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: &imap.SeqSet{Set: []imap.Seq{{2, 2}}},
+					Not: []*imap.SearchCriteria{{SeqNum: new(imap.SeqSet)}},
+				},
+				{
+					SeqNum: new(imap.SeqSet),
+				},
+			}},
+		},
+		res: true,
+	},
+	{
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: &imap.SeqSet{Set: []imap.Seq{{2, 2}}},
+					Not: []*imap.SearchCriteria{{
+						SeqNum: &imap.SeqSet{Set: []imap.Seq{imap.Seq{1, 1}}},
+					}},
+				},
+				{
+					SeqNum: new(imap.SeqSet),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: &imap.SeqSet{Set: []imap.Seq{{2, 2}}},
+					Not: []*imap.SearchCriteria{{
+						SeqNum: &imap.SeqSet{Set: []imap.Seq{{1, 1}}},
+					}},
+				},
+				{
+					SeqNum: &imap.SeqSet{Set: []imap.Seq{{1, 1}}},
+				},
+			}},
+		},
+		res: true,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(-48 * time.Hour),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(-48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(-48 * time.Hour),
+				},
+			}},
+		},
+		res: true,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(-48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(-48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(-48 * time.Hour),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(-48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(-48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(48 * time.Hour),
+				},
+			}},
+		},
+		res: true,
+	},
 }
 
-func Mailbox_SearchMessages_Body(t *testing.T, newBack NewBackFunc, closeBack CloseBackFunc) {
+func Mailbox_SearchMessages(t *testing.T, newBack NewBackFunc, closeBack CloseBackFunc) {
 	b := newBack()
 	defer closeBack(b)
-	err := b.CreateUser("username1", "password1")
-	assert.NilError(t, err)
-	u, err := b.GetUser("username1")
-	assert.NilError(t, err)
+	u := getUser(t, b)
 	defer assert.NilError(t, u.Logout())
 
-	assert.NilError(t, u.CreateMailbox("TEST"))
-	mbox, err := u.GetMailbox("TEST")
-	assert.NilError(t, err)
-
-	assert.NilError(t, mbox.CreateMessage([]string{"$Test1", "$Test2"}, testDate, strings.NewReader(testMailString)))
-
-	for _, crit := range matchTests {
-		crit := crit
-		t.Run("Crit "+crit.name, func(t *testing.T) {
+	for i, test := range matchTests {
+		test := test
+		t.Run("Crit "+strconv.Itoa(i+1), func(t *testing.T) {
 			skipIfExcluded(t)
+			mbox := getMbox(t, u)
 
-			if crit.name == "SentSince" {
-				t.Skip("Skipped due to bug in go-imap (https://github.com/emersion/go-imap/issues/222)")
-				t.SkipNow()
-			}
+			// Create a message and delete it to make sure test message will have seqnum=1 and uid=2.
+			assert.NilError(t, mbox.CreateMessage(test.flags, test.date, strings.NewReader(testMailString)))
+			assert.NilError(t, mbox.UpdateMessagesFlags(false, &imap.SeqSet{Set: []imap.Seq{{1, 1}}}, imap.AddFlags, []string{imap.DeletedFlag}))
+			assert.NilError(t, mbox.Expunge())
 
-			res, err := mbox.SearchMessages(false, crit.criteria)
+			assert.NilError(t, mbox.CreateMessage(test.flags, test.date, strings.NewReader(testMailString)))
+
+			res, err := mbox.SearchMessages(false, test.criteria)
 			assert.NilError(t, err)
-			if crit.res {
-				assert.Assert(t, len(res) == 1 && res[0] == 1, "Criteria not matched when expected")
+			if test.res {
+				if !assert.Check(t, len(res) == 1 && res[0] == 1, "Criteria not matched when expected") {
+					t.Logf("Criteria: %+v\n", test.criteria)
+					t.Logf("Res: %+v\n", res)
+				}
 			} else {
-				assert.Assert(t, len(res) == 0, "Criteria matched when not expected")
-			}
-		})
-	}
-}
-
-func Mailbox_SearchMessages_Flags(t *testing.T, newBack NewBackFunc, closeBack CloseBackFunc) {
-	b := newBack()
-	defer closeBack(b)
-	err := b.CreateUser("username1", "password1")
-	assert.NilError(t, err)
-	u, err := b.GetUser("username1")
-	assert.NilError(t, err)
-	defer assert.NilError(t, u.Logout())
-
-	for i, crit := range flagsTests {
-		crit := crit
-		name := "Crit " + strconv.Itoa(i+1)
-		t.Run(name, func(t *testing.T) {
-			skipIfExcluded(t)
-
-			assert.NilError(t, u.CreateMailbox("TEST"+name))
-			mbox, err := u.GetMailbox("TEST" + name)
-			assert.NilError(t, err)
-
-			assert.NilError(t, mbox.CreateMessage(crit.flags, testDate, strings.NewReader(testMailString)))
-
-			res, err := mbox.SearchMessages(false, crit.criteria)
-			assert.NilError(t, err)
-			if crit.res {
-				assert.Assert(t, len(res) == 1 && res[0] == 1, "Criteria not matched when expected")
-			} else {
-				assert.Assert(t, len(res) == 0, "Criteria matched when not expected")
+				if !assert.Check(t, len(res) == 0, "Criteria matched when not expected") {
+					t.Logf("Criteria: %+v\n", test.criteria)
+					t.Logf("Res: %+v\n", res)
+				}
 			}
 		})
 	}
